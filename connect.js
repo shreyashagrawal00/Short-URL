a5a3acb60e0d3779d51a9de2eba836a8330e66dd
Shreyash Agrawal <shreyash.agrawal3004@gmail.com>
2026-01-20 23:26:40 +0530

    fixing bugs

--- connect.js
+++ connect.js
@@ -1,7 +1,21 @@
 const mongoose = require('mongoose');
 
-async function connectToMongoDB(url){
-  return mongoose.connect(url);
+let isConnected = false;
+
+async function connectToMongoDB(url) {
+  if (isConnected) {
+    console.log('Using existing MongoDB connection');
+    return;
+  }
+
+  try {
+    await mongoose.connect(url);
+    isConnected = true;
+    console.log('New MongoDB connection established');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    throw error;
+  }
 }
 
 module.exports = {
